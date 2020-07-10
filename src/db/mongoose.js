3786215bd33e2404f23584246eeb8e54b0098734
Shubham Rawat <47300890+Rawattt@users.noreply.github.com>
2020-07-11 01:11:07 +0530

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,6 +5,6 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology:true,
-    useFindAndModify:false, // To resolve DeprecationWarning: Mongoose: `findOneAndUpdate()` and `findOneAndDelete()` without the `useFindAndModify` option set to false are deprecated
-    autoIndex:true  // For unique email
+    useFindAndModify:false,
+    autoIndex:true
 })
